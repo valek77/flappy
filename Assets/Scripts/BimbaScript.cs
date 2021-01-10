ffdf627a69d1cb19aa8005f0509cf6bcdc2f3e71
Marco Zamparelli <wbarlowe77@gmail.com>
2021-01-10 19:10:27 +0100

    Movimenti Bimba

---
+++ Assets/Scripts/BimbaScript.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BimbaScript : MonoBehaviour
+{
+
+    public float Forza;
+
+     Rigidbody2D rb;
+    bool partito;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        partito = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0)) {
+
+            if (!partito)
+            {
+                partito = true;
+                rb.isKinematic = false;
+
+            }
+            else 
+            {
+                rb.AddForce(new Vector2(0,Forza));
+            }
+
+        }
+    }
+}
